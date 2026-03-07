f1208d6dbf474816551c11c1d14812f175ad6c16
ShadowThijs <logtenbergthijs@yahoo.com>
2026-03-07 18:46:54 +0100

    fix clang build linux

--- src/commands/cmd_watch.c
+++ src/commands/cmd_watch.c
@@ -5,9 +5,11 @@
 #include <windows.h>
 #else
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <dirent.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <time.h>
 #endif
 
 /* -------------------------------------------------------------------------
